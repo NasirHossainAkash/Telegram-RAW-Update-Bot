f22ff2140a87a06a4da467f0e56ebfb9f5c1be88
NasirHossainAkash <nasirhossainakash5@gmail.com>
2022-07-02 22:32:25 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# Telegram RAW Update Bot
+
+A Telegram bot to get all the telegram update 
+
+
+## Features
+
+- Get RAW Update Details on Private
+- Group Supported
+
+## LICENSE
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+
+
+
+## Screenshots
+
+![](https://i.ibb.co/kD95NhS/image-2022-07-02-22-01-17.png)
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables 
+
+`API_ID`
+
+`API_HASH`
+
+`BOT_TOKEN`
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  pip install -r requirements.txt
+  python3 main.py
+```
+
+
+## Author
+
+- [@NasirHossainAkash](https://www.github.com/NasirHossainAkash)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from pyrogram import Client, filters
+import logging
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+app = Client(name="rawupdate", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+
+@app.on_message(filters.command("start"))
+async def start_handler(client, message):
+    logging.info("start_handler executed")
+    await client.send_message(
+        chat_id=message.from_user.id,
+        text=f"**Hello {message.from_user.first_name},\nI am a Telegram RAW Update Bot\n\n✅ Developed By: @PrimeAkash**",
+    )
+    logging.info(f"{message.from_user.first_name} started the bot")
+
+
+@app.on_message()
+async def raw_handler(client, message):
+    logging.info("raw update handler executed")
+    await client.send_message(
+        chat_id=message.from_user.id,
+        text=f"**Telegram RAW Update**\n\n```{message}```\n\n**✅Developed By: @PrimeAkash**",
+    )
+    logging.info(f"{message.from_user.first_name} got the raw update")
+
+
+app.run()
